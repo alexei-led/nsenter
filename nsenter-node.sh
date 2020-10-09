c56838f145fb9d544e2bde34c14dc63b61e0ffa7
Kimmo Ahokas <kimmo.ahokas@futurice.com>
2020-10-09 07:34:49 +0300

    fix illegal characters in podName

--- nsenter-node.sh
+++ nsenter-node.sh
@@ -2,9 +2,15 @@
 set -x
 
 node=${1}
-nodeName=$(kubectl get node ${node} -o template --template='{{index .metadata.labels "kubernetes.io/hostname"}}') 
+nodeName=$(kubectl get node ${node} -o template --template='{{index .metadata.labels "kubernetes.io/hostname"}}')
 nodeSelector='"nodeSelector": { "kubernetes.io/hostname": "'${nodeName:?}'" },'
 podName=${USER}-nsenter-${node}
+# convert @ to -
+podName=${podName//@/-}
+# convert . to -
+podName=${podName//./-}
+# truncate podName to 63 characters which is the kubernetes max length for it
+podName=${podName:0:63}
 
 kubectl run ${podName:?} --restart=Never -it --rm --image overriden --overrides '
 {
